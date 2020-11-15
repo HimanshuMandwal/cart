961da85aa2327056e1fdcef5d3bc579cd5fa8751
himanshu mandwal <hkushwah8@gmail.com>
2020-11-15 14:32:32 +0530

    added basic icons and defined the basic state for the current page

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,19 +1,31 @@
 import React from 'react';
 //this is a class based component whereas our App() is a functional component
 class CartItem extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      price : 2000,
+      title: 'phone',
+      Qty : 1,
+      img: ''
+    };
+  }
   render(){
+    const { price,title,Qty,img } = this.state;//here we have used object destructuring
     return (
       <div className='cart-item'>
         <div className='left-block'>
-        <img style={styles.image}/>
+        <img style={styles.image} />
         </div>
         <div className='right-block'>
-          <div style={{ fontSize:25}}>Phone</div>
-          <div style={{ fontSize:25, color:'#777'}}> Rs 23000</div>
-          <div style={{ fontSize:25, color:'#777'}}>Qty: 1</div>
+    <div style={{ fontSize:25}}>{title}</div>
+    <div style={{ fontSize:25, color:'#777'}}> Rs {price}</div>
+    <div style={{ fontSize:25, color:'#777'}}>Qty: {Qty}</div>
           <div className='cart-item-actions'>
           {/* Buttons */}
-
+          <img alt='increase' className='action-icons' src="https://www.flaticon.com/svg/static/icons/svg/659/659892.svg"/>
+          <img alt='decrease' className='action-icons' src="https://www.flaticon.com/svg/static/icons/svg/1828/1828926.svg"/>
+          <img alt='delete' className='action-icons' src="https://www.flaticon.com/svg/static/icons/svg/3096/3096673.svg"/>
           </div>
         </div>
       </div>
